447f93a55264d82930402a3953f319d0146f30bc
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-05-18 23:50:35 +0100

    resolved 404 error

--- src/main/java/dev/abrahambankole/content_calendar/controller/ContentController.java
+++ src/main/java/dev/abrahambankole/content_calendar/controller/ContentController.java
@@ -4,6 +4,7 @@ import dev.abrahambankole.content_calendar.model.Content;
 import dev.abrahambankole.content_calendar.repository.ContentCollectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +26,6 @@ public class ContentController {
     }
 
     // Create Read Update Delete - filter | paging | sorting
+    @GetMapping("/{id}")
+    public Optional<Content> findById(@PathVariable Long id) {}
 }
