fa8ed66ea432a7a224f41b4927949e6028280c29
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-05-18 19:25:28 +0100

    added config file

---
+++ src/main/java/dev/abrahambankole/content_calendar/Application.java
@@ -0,0 +1,16 @@
+package dev.abrahambankole.content_calendar;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	// write more code
+}
---
+++ src/main/java/dev/abrahambankole/content_calendar/Message.java
@@ -0,0 +1,10 @@
+package dev.abrahambankole.content_calendar;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+    public String  getMessage() {
+        return "Hello World!";
+    }
+}
