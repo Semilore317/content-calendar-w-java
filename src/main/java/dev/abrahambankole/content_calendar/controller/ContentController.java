2c443d03bde0158d89936aa1346b2a3c726801b3
Abraham Oluwasemilore Bankole <semi.banks.317@gmail.com>
2025-05-19 19:38:45 +0100

    handled 404 errors and added spring-boot-starter-test dependency

--- src/main/java/dev/abrahambankole/content_calendar/controller/ContentController.java
+++ src/main/java/dev/abrahambankole/content_calendar/controller/ContentController.java
@@ -3,12 +3,12 @@ package dev.abrahambankole.content_calendar.controller;
 import dev.abrahambankole.content_calendar.model.Content;
 import dev.abrahambankole.content_calendar.repository.ContentCollectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
@@ -27,5 +27,12 @@ public class ContentController {
 
     // Create Read Update Delete - filter | paging | sorting
     @GetMapping("/{id}")
-    public Optional<Content> findById(@PathVariable Long id) {}
+    public Content findByID(@PathVariable Integer id){
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found"));
+    }
+
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
 }
--- src/main/java/dev/abrahambankole/content_calendar/repository/ContentCollectionRepository.java
+++ src/main/java/dev/abrahambankole/content_calendar/repository/ContentCollectionRepository.java
@@ -13,22 +13,26 @@ import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository() {
     }
 
     public List<Content> findAll() {
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id) {
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    public void save(Content content) {
+        contentList.add(content);
     }
 
     @PostConstruct
     private void init() {
-        Content c = new Content( 1,
+        Content content = new Content( 1,
                 "My First Blog Post",
                 "First Blog Post",
                 Status.IDEA,
@@ -36,6 +40,6 @@ public class ContentCollectionRepository {
                 LocalDateTime.now(),
                 null,
                 "");
-        content.add(c);
+        contentList.add(content);
     }
 }
